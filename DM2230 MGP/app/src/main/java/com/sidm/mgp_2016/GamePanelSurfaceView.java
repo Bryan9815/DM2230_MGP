21db7e254f7b77d30f64f7a8a1687acd8d937d2c
Bryan <btteh@hotmail.com>
2016-12-04 18:00:52 +0800

    Moved blue_button to drawables folder, updated GamePanelSurfaceView to week 7, added SpriteAnimation

--- DM2230 MGP/app/src/main/java/com/sidm/mgp_2016/GamePanelSurfaceView.java
+++ DM2230 MGP/app/src/main/java/com/sidm/mgp_2016/GamePanelSurfaceView.java
@@ -4,46 +4,60 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Space;
+import android.graphics.Paint;
 
-/**
- * Created by Alienware Pc on 021, 21-Nov-2016.
- */
-public class GamePanelSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
+import java.util.Random;
+
+public class GamePanelSurfaceView extends SurfaceView implements SurfaceHolder.Callback
+{
     // Implement this interface to receive information about changes to the surface.
+
+    protected static final String TAG = null;
 
     private GameThread myThread = null; // Thread to control the rendering
 
-    // 1a) Variables used for background rendering
-    private Bitmap Background;
-    private Bitmap ScaledBackground;
-    // 1b) Define Screen width and Screen height as integer
+    // 1) Variables used for background rendering
+    private Bitmap bg, scaledbg;
+
+    //Stone Animation
+    private SpriteAnimation stone_anim;
+
     int ScreenWidth, ScreenHeight;
-    // 1c) Variables for defining background start and end point
-    private short BackgroundX = 0, BackgroundY = 0;
-    // 4a) bitmap array to stores 4 images of the spaceship
-    private Bitmap[] SpaceShip = new Bitmap[4];
-    // 4b) Variable as an index to keep track of the spaceship images
-    private short SpaceShipIndex = 0;
 
-    // Spaceship Location
-    int ShipX = 0, ShipY = 0;
-
-    // Variables for FPS
     public float FPS;
-    float deltaTime;
     long dt;
 
-    // Variable for Game State check
     private short GameState;
 
+    // Paint object
+    Paint paint = new Paint();
+
+    private short bgX = 0, bgY = 0;
+
+    private short mX = 100, mY = 0;
+
+    private int aX = 300, aY = 300;
+
+    // 5) bitmap array to stores 4 images of the spaceship
+    private Bitmap[] ship = new Bitmap[4];
+
+    // 6) Variable as an index to keep track of the spaceship images
+    private short shipIndex = 0;
+
+    //Week 7 to move move ship
+    private boolean moveShip = false;
+
     //constructor for this GamePanelSurfaceView class
-    public GamePanelSurfaceView (Context context){
+    public GamePanelSurfaceView(Context context)
+    {
 
         // Context is the current state of the application/object
         super(context);
@@ -51,22 +65,23 @@ public class GamePanelSurfaceView extends SurfaceView implements SurfaceHolder.C
         // Adding the callback (this) to the surface holder to intercept events
         getHolder().addCallback(this);
 
-        // 1d) Set information to get screen size
-        DisplayMetrics Metrics  = context.getResources().getDisplayMetrics();
-        ScreenHeight = Metrics.heightPixels;
-        ScreenWidth = Metrics.widthPixels;
+        // Set things to get screen size
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        ScreenWidth = metrics.widthPixels;
+        ScreenHeight = metrics.heightPixels;
 
-        // 1e)load the image when this class is being instantiated
-        Background = BitmapFactory.decodeResource(getResources(), R.drawable.gamescene);
-        ScaledBackground = Bitmap.createScaledBitmap(Background, ScreenWidth, ScreenHeight, true);
-        // 4c) Load the images of the spaceships
-        SpaceShip[0] = BitmapFactory.decodeResource(getResources(), R.drawable.ship2_1);
-        SpaceShip[1] = BitmapFactory.decodeResource(getResources(), R.drawable.ship2_2);
-        SpaceShip[2] = BitmapFactory.decodeResource(getResources(), R.drawable.ship2_3);
-        SpaceShip[3] = BitmapFactory.decodeResource(getResources(), R.drawable.ship2_4);
+        // 2)load the image when this class is being instantiated
+        bg = BitmapFactory.decodeResource(getResources(), R.drawable.gamescene);
+        scaledbg = Bitmap.createScaledBitmap(bg, (int) (ScreenWidth), (int) (ScreenHeight), true);
 
-        SpaceShip[0] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ship2_1), ScreenWidth /10, ScreenWidth/10, true);
+        // 7) Load the images of the spaceships
+        ship[0] = Bitmap.createScaledBitmap((BitmapFactory.decodeResource(getResources(), R.drawable.ship2_1)), (int) (ScreenWidth) / 10, (int) (ScreenHeight) / 10, true);
+        ship[1] = Bitmap.createScaledBitmap((BitmapFactory.decodeResource(getResources(), R.drawable.ship2_2)), (int) (ScreenWidth) / 10, (int) (ScreenHeight) / 10, true);
+        ship[2] = Bitmap.createScaledBitmap((BitmapFactory.decodeResource(getResources(), R.drawable.ship2_3)), (int) (ScreenWidth) / 10, (int) (ScreenHeight) / 10, true);
+        ship[3] = Bitmap.createScaledBitmap((BitmapFactory.decodeResource(getResources(), R.drawable.ship2_4)), (int) (ScreenWidth) / 10, (int) (ScreenHeight) / 10, true);
 
+        // Week 7 Load images for Flying Stone animation
+        //stone_anim = new SpriteAnimation(Bitmap.createScaledBitmap((BitmapFactory.decodeResource(getResources(), R.drawable.flystar)), (int) (ScreenWidth)/4, (int) (ScreenHeight)/10, true), 320, 64, 5, 5);
 
         // Create the game loop thread
         myThread = new GameThread(getHolder(), this);
@@ -76,102 +91,183 @@ public class GamePanelSurfaceView extends SurfaceView implements SurfaceHolder.C
     }
 
     //must implement inherited abstract methods
-    public void surfaceCreated(SurfaceHolder holder){
+    public void surfaceCreated(SurfaceHolder holder)
+    {
         // Create the thread
-        if (!myThread.isAlive()){
+        if (!myThread.isAlive())
+        {
             myThread = new GameThread(getHolder(), this);
             myThread.startRun(true);
             myThread.start();
         }
     }
 
-    public void surfaceDestroyed(SurfaceHolder holder){
-        // Destroy the thread
-        if (myThread.isAlive()){
-            myThread.startRun(false);
-
-
-        }
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
         boolean retry = true;
-        while (retry) {
-            try {
+
+        // Destroy the thread
+        if (myThread.isAlive())
+        {
+            myThread.startRun(false);
+        }
+
+        // Stop the thread
+        while (retry)
+        {
+            try
+            {
                 myThread.join();
                 retry = false;
-            }
-            catch (InterruptedException e)
+            } catch (InterruptedException e)
             {
+
             }
         }
     }
 
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
-
-    }
-
-    public void RenderGameplay(Canvas Canvas) {
-        // 2) Re-draw 2nd image after the 1st image ends
-        if (Canvas == null)
-        {
-            return;
-        }
-        Canvas.drawBitmap(ScaledBackground, BackgroundX, BackgroundY, null);
-        Canvas.drawBitmap(ScaledBackground, BackgroundX + ScreenWidth, BackgroundY, null);
-        // 4d) Draw the spaceships
-        Canvas.drawBitmap(SpaceShip[SpaceShipIndex], BackgroundX, BackgroundY, null);
-
-
-        // Bonus) To print FPS on the screen
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
+    {
 
     }
 
 
-    //Update method to update the game play
-    public void update(float dt, float fps){
+    //public void update(){
+    public void update(float dt, float fps)
+    {
         FPS = fps;
 
-        switch (GameState) {
-            case 0: {
-                // 3) Update the background to allow panning effect
-                BackgroundX -= 500 * dt; // Change pan speed
-                if (BackgroundX < -ScreenWidth)
+        switch (GameState)
+        {
+            case 0:
+            {
+                // 4) An update function to update the game
+                bgX -= 500 * dt; //Change the number of panning speed
+                if (bgX < -ScreenWidth)
                 {
-                    BackgroundX = 0;
+                    bgX = 0;
                 }
+                // 9) Update the spaceship images / shipIndex so that the animation will occur.
+                shipIndex++;
+                shipIndex %= 4;
 
+                // Draw stone animation
+                //stone_anim.update(System.currentTimeMillis());
 
-                // 4e) Update the spaceship images / shipIndex so that the animation will occur.
-                SpaceShipIndex++;
-                if (SpaceShipIndex > 3)
-                {
-                    SpaceShipIndex = 0;
-                }
+                break;
             }
-            break;
         }
     }
 
     // Rendering is done on Canvas
-    public void doDraw(Canvas canvas){
+    public void doDraw(Canvas canvas)
+    {
+
         switch (GameState)
         {
             case 0:
                 RenderGameplay(canvas);
+
                 break;
+
         }
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-
-        // 5) In event of touch on screen, the spaceship will relocate to the point of touch
-        short x = (short)event.getX();
-        short y = (short)event.getY();
-
-        if (event.getAction() == MotionEvent.ACTION_DOWN)
+    public void RenderGameplay(Canvas canvas)
+    {
+        // 3) Re-draw 2nd image after the 1st image ends
+        if (canvas == null)
         {
-            ShipX = (short) (x - SpaceShip[SpaceShipIndex].getWidth()*0.5f);
-            ShipY = (short) (y - SpaceShip[SpaceShipIndex].getHeight()*0.5f);
+            return;
         }
-        return super.onTouchEvent(event);
+        canvas.drawBitmap(scaledbg, bgX, bgY, null);
+        canvas.drawBitmap(scaledbg, bgX + ScreenWidth, bgY, null);
+
+        // 8) Draw the spaceships
+        canvas.drawBitmap(ship[shipIndex], mX, mY, null);
+
+        // Week 7 Render FPS
+        RenderTextonScreen(canvas,"FPS: " + FPS, 130, 75, 30);
+
+        // Draw sprite
+        stone_anim.draw(canvas);
+
+        //Week 7
+        //Edit after adding collision onTouch event & comment code above  -- stone_anim.setY(600);
+        stone_anim.setX(aX);
+        stone_anim.setY(aY);
+    }
+
+    // Week 7 Print text on screen
+    public void RenderTextonScreen (Canvas canvas, String text, int posX, int posY, int textsize)
+    {
+        if (canvas != null && text.length() != 0)
+        {
+            Paint paint = new Paint();
+            paint.setARGB(255, 0, 0, 0);
+            paint.setStrokeWidth(100);
+            paint.setTextSize(textsize);
+            canvas.drawText(text, posX, posY, paint);
+        }
+    }
+    // Week 7 AABB
+    public boolean CheckCollision (int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2)
+    {
+        //top left
+        if (x2 >= x1 && x2 <= x1 + w1)
+        {
+            if (y2 >= y1 && y2 <= y1 + h1)
+                return true;
+        }
+        //top right
+        if (x2 + w2 >= x1 && x2 + w2 <= x1 + w1)
+        {
+            if (y2 >= y1 && y2 <= y1 + h1)
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        int action = event.getAction();// Check for the action of touch
+
+        short X = (short) event.getX();
+        short Y = (short) event.getY();
+
+
+        switch(action)
+        {
+            case MotionEvent.ACTION_DOWN:
+                if (CheckCollision(mX, mY, ship[shipIndex].getWidth(), ship[shipIndex].getHeight(), X, Y, 0, 0))
+                {
+                    moveShip = true;
+                } else
+                {
+                    moveShip = false;
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (moveShip == true)
+                {
+                    // New location where the image to land on
+                    mX = (short) (X - ship[shipIndex].getWidth() / 2);
+                    mY = (short) (Y - ship[shipIndex].getHeight() / 2);
+                }
+                //Check if ship and stone collide
+                if (CheckCollision(mX, mY, ship[shipIndex].getWidth(), ship[shipIndex].getHeight(), aX, aY, stone_anim.getSpriteWidth(), stone_anim.getSpriteHeight()))
+                {
+                    Random random = new Random();
+
+                    aX = random.nextInt(ScreenWidth-50);
+                    aY = random.nextInt(ScreenHeight-50);
+                }
+                break;
+
+            case MotionEvent.ACTION_UP:
+
+                break;
+        }return true;
     }
 }
